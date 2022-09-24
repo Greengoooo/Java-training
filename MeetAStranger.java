e6166a3aeca33343c2415b812230877ad597907b
Greengoooo <103311340+Greengoooo@users.noreply.github.com>
2022-09-24 09:25:21 +0300

    Add files via upload

---
+++ MeetAStranger.java
@@ -0,0 +1,9 @@
+package com.epam.rd.autotasks.meetastranger;
+
+import java.util.Scanner;
+
+public class LearnArithmetic {
+    public static void main(String args[]) {
+        System.out.println(12345 + 5432l);
+    }
+}
