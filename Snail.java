1153dbe9961723e4d6842fd0963e7f12028a5d5e
Greengoooo <103311340+Greengoooo@users.noreply.github.com>
2022-09-24 09:28:38 +0300

    Add files via upload

--- Snail.java
+++ Snail.java
@@ -2,37 +2,31 @@ package com.epam.rd.autotasks.snail;
 
 import java.util.Scanner;
 
-public class Snail
-{
-    public static void main(String[] args)
-    {
-
+public class Snail {
+    public static void main(String[] args) {
 
         Scanner scanner1 = new Scanner(System.in);
-        int a =  scanner1.nextInt();
-
-        Scanner scanner2 = new Scanner(System.in);
-        int b =  scanner2.nextInt();
-
-        Scanner scanner3 = new Scanner(System.in);
-        int h = scanner3.nextInt();
-
+        int a = scanner1.nextInt();
+        int b = scanner1.nextInt();
+        int h = scanner1.nextInt();
+        int position = 0;
+        int countDay = 0;
         if (a >= h) {
-            System.out.println(1);
-        } else if (a<=b) {
+            System.out.println("1");
+            return;
+        } else if (b >= a) {
             System.out.println("Impossible");
-        } else {
-            int c = a - b;
-            int r = (h - a) / c;
-            System.out.println(Math.round(r + 1));
+            return;
         }
-
-
-
-
-
-
-
+        while (true) {
+            position += a;
+            countDay++;
+            if (position >= h) {
+                break;
+            }
+            position -= b;
+        }
+        System.out.println(countDay);
 
 
 
